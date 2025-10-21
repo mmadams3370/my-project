e6fe3c95496c93dc7cb3e7ed7a90fefd0784a1cb
mmadams3370 <68846864+mmadams3370@users.noreply.github.com>
2025-10-21 15:02:01 -0700

    Fix PHP opening tag syntax in index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ if (!isset($_SESSION['logged_in']) || $_SESSION['logged_in'] !== true) {
     header("Location: login.php");
     exit();
 }
-<?php
+<"?php"
 function forecast_sales($sales, $steps = 5, $seasonality = 7) {
     $window = 3; // moving average window
     $forecasts = [];
